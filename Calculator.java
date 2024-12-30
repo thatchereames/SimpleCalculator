8aa4bcecb75a81d020c4c7fa59804b68550d0ce1
Thatcher Eames <142461236+thatchereames@users.noreply.github.com>
2024-12-30 03:30:04 +0000

    JavaDoc comments

--- Calculator.java
+++ Calculator.java
@@ -1,10 +1,20 @@
 /**
- * Calculates values based on user input. 
+ * Creater: Thatcher Eames
+ * Email: thatcher.eames@gmail.com
+ * File Name: Calculator
+ * 
+ * This file creates a typing calculator. Users simpling type in the
+ * calculations they wish to make following the format guidelines provided. 
  */
 import java.util.Scanner;
 
+/**
+ * This class is a calculator that parses a string input from the user into data
+ * that is calculated. The string input is parsed based on a format given to
+ * the user.
+ */
 public class Calculator {
-    //Instance Variables
+    //Helper varibables
     private static final String CALCULATION_KEY = "Type + - * / () ^ ! for "
     + " their typical calculations.\nType , to denote a fraction\nType quit at " 
     + "any time to exit the program. Type help to view this menu again.";
@@ -16,6 +26,7 @@ public class Calculator {
         boolean contin = true;
         int numOfCalculations = 1;
         String calculation;
+        //Continues asking for input and calculating until the user exits
         while (contin) {
             System.out.print("Calculation " + numOfCalculations + ": ");
             calculation = myScanner.nextLine();
